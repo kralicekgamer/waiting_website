6222ad34627f422c7fabfc7ca0f609f1a424fce4
Matěj Adamec <kralicekgamer@gmail.com>
2024-10-22 18:26:05 +0200

    Builded website

---
+++ script.js
@@ -0,0 +1,16 @@
+let totalSeconds = 0;
+
+function formatTime(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+}
+
+function updateTimer() {
+    totalSeconds++;
+    document.getElementById("time").textContent = formatTime(totalSeconds);
+}
+
+setInterval(updateTimer, 1000); 
